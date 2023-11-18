b61736ca291d6e22007883b3f344a7badaf365c0
holbroow <willholbrook10@gmail.com>
2023-11-18 20:11:57 +0000

    Reads and copies entire bootsector to fields.

--- program.c
+++ program.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 typedef struct __attribute__((__packed__)) {
     u_int8_t BS_jmpBoot[ 3 ]; // x86 jump instr. to boot code
@@ -30,28 +31,65 @@ typedef struct __attribute__((__packed__)) {
 
 
 int main() {
-    int fd; // fat16 image
+    int fd; // the fat16 image
     off_t offset = 0; // how many bytes from the start of the file to begin
-    size_t bytesToRead = 3; // how many bytes to be read from the file
-    ssize_t bytesRead; // how many bytes were actually read
-    char buffer[100]; // the character buffer for the read bytes
+    size_t bytesToRead = sizeof(BootSector); // how many bytes to be read from the file
+    ssize_t bytesRead; // how many bytes were actually read from the file
+    char buffer[bytesToRead]; // buffer where the file bytes are temporarily read to
+    BootSector bootSector; // declaration of the BootSector struct.
 
 
     // opens file
-    fd = open("fat16.img", O_RDONLY);                   
+    fd = open("fat16.img", O_RDONLY);
 
-    // seeks to the byte position in the file
-    lseek(fd, offset, SEEK_SET);                        
+    // seeks to the start of the file for the bootsector
+    lseek(fd, offset, SEEK_SET);
 
-    // reads the bytes at that position
-    bytesRead = read(fd, buffer, bytesToRead);         
+    // reads the file from start the the end of the boot sector
+    bytesRead = read(fd, buffer, bytesToRead);
+    
+    // copies its contents to the correct struct fields
+    memcpy(&bootSector, buffer, bytesToRead);
 
-    // prints said bytes
-    printf("Bytes read: \n");                           
-    for (size_t i = 0; i < bytesRead; i++) {
-        printf("%02X, ", (unsigned char)buffer[i]); // print individual byte itself
+
+    // prints said bytes (for testing)
+    printf("BS_jmpBoot: ");
+    for (size_t i = 0; i < sizeof(bootSector.BS_jmpBoot); i++) {
+        printf("%02X, ", bootSector.BS_jmpBoot[i]);
     }
     printf("\n");
+    printf("BS_OEMName: ");
+    for (size_t i = 0; i < sizeof(bootSector.BS_OEMName); i++) {
+        printf("%02X, ", bootSector.BS_OEMName[i]);
+    }
+    printf("\n");
+    printf("BPB_BytsPerSec: %02X\n", bootSector.BPB_BytsPerSec);
+    printf("BPB_SecPerClus: %02X\n", bootSector.BPB_SecPerClus);
+    printf("BPB_RsvdSecCnt: %02X\n", bootSector.BPB_RsvdSecCnt);
+    printf("BPB_NumFATs: %02X\n", bootSector.BPB_NumFATs);
+    printf("BPB_RootEntCnt: %02X\n", bootSector.BPB_RootEntCnt);
+    printf("BPB_TotSec16: %02X\n", bootSector.BPB_TotSec16);
+    printf("BPB_Media: %02X\n", bootSector.BPB_Media);
+    printf("BPB_FATSz16: %02X\n", bootSector.BPB_FATSz16);
+    printf("BPB_SecPerTrk: %02X\n", bootSector.BPB_SecPerTrk);
+    printf("BPB_NumHeads: %02X\n", bootSector.BPB_NumHeads);
+    printf("BPB_HiddSec: %02X\n", bootSector.BPB_HiddSec);
+    printf("BPB_TotSec32: %02X\n", bootSector.BPB_TotSec32);
+    printf("BS_DrvNum: %02X\n", bootSector.BS_DrvNum);
+    printf("BS_Reserved1: %02X\n", bootSector.BS_Reserved1);
+    printf("BS_BootSig: %02X\n", bootSector.BS_BootSig);
+    printf("BS_VolID: %02X\n", bootSector.BS_VolID);
+    printf("BS_VolLab: ");
+    for (size_t i = 0; i < sizeof(bootSector.BS_VolLab); i++) {
+        printf("%02X, ", bootSector.BS_VolLab[i]);
+    }
+    printf("\n");
+    printf("BS_FilSysType: ");
+    for (size_t i = 0; i < sizeof(bootSector.BS_VolLab); i++) {
+        printf("%02X, ", bootSector.BS_FilSysType[i]);
+    }
+    printf("\n");
+
 
     // close file
     close(fd);
