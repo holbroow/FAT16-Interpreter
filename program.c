09ddaa79b682ca7fee0abde6dbc61d6ebbed3404
Will Holbrook <willholbrook10@gmail.com>
2023-11-19 00:06:27 +0000

    Made changes to comment structure

--- program.c
+++ program.c
@@ -7,52 +7,45 @@
 #include <string.h>
 
 typedef struct __attribute__((__packed__)) {
-    u_int8_t BS_jmpBoot[ 3 ]; // x86 jump instr. to boot code
-    u_int8_t BS_OEMName[ 8 ]; // What created the filesystem
-    u_int16_t BPB_BytsPerSec; // Bytes per Sector
-    u_int8_t BPB_SecPerClus; // Sectors per Cluster
-    u_int16_t BPB_RsvdSecCnt; // Reserved Sector Count
-    u_int8_t BPB_NumFATs; // Number of copies of FAT
-    u_int16_t BPB_RootEntCnt; // FAT12/FAT16: size of root DIR
-    u_int16_t BPB_TotSec16; // Sectors, may be 0, see below
-    u_int8_t BPB_Media; // Media type, e.g. fixed
-    u_int16_t BPB_FATSz16; // Sectors in FAT (FAT12 or FAT16)
-    u_int16_t BPB_SecPerTrk; // Sectors per Track
-    u_int16_t BPB_NumHeads; // Number of heads in disk
-    u_int32_t BPB_HiddSec; // Hidden Sector count
-    u_int32_t BPB_TotSec32; // Sectors if BPB_TotSec16 == 0
-    u_int8_t BS_DrvNum; // 0 = floppy, 0x80 = hard disk
-    u_int8_t BS_Reserved1; //
-    u_int8_t BS_BootSig; // Should = 0x29
-    u_int32_t BS_VolID; // 'Unique' ID for volume
-    u_int8_t BS_VolLab[ 11 ]; // Non zero terminated string
-    u_int8_t BS_FilSysType[ 8 ]; // e.g. 'FAT16 ' (Not 0 term.)
+    u_int8_t BS_jmpBoot[ 3 ];       // x86 jump instr. to boot code
+    u_int8_t BS_OEMName[ 8 ];       // What created the filesystem
+    u_int16_t BPB_BytsPerSec;       // Bytes per Sector
+    u_int8_t BPB_SecPerClus;        // Sectors per Cluster
+    u_int16_t BPB_RsvdSecCnt;       // Reserved Sector Count
+    u_int8_t BPB_NumFATs;           // Number of copies of FAT
+    u_int16_t BPB_RootEntCnt;       // FAT12/FAT16: size of root DIR
+    u_int16_t BPB_TotSec16;         // Sectors, may be 0, see below
+    u_int8_t BPB_Media;             // Media type, e.g. fixed
+    u_int16_t BPB_FATSz16;          // Sectors in FAT (FAT12 or FAT16)
+    u_int16_t BPB_SecPerTrk;        // Sectors per Track
+    u_int16_t BPB_NumHeads;         // Number of heads in disk
+    u_int32_t BPB_HiddSec;          // Hidden Sector count
+    u_int32_t BPB_TotSec32;         // Sectors if BPB_TotSec16 == 0
+    u_int8_t BS_DrvNum;             // 0 = floppy, 0x80 = hard disk
+    u_int8_t BS_Reserved1;          //
+    u_int8_t BS_BootSig;            // Should = 0x29
+    u_int32_t BS_VolID;             // 'Unique' ID for volume
+    u_int8_t BS_VolLab[ 11 ];       // Non zero terminated string
+    u_int8_t BS_FilSysType[ 8 ];    // e.g. 'FAT16 ' (Not 0 term.)
 } BootSector;
 
 
 int main() {
-    int fd; // the fat16 image
-    off_t offset = 0; // how many bytes from the start of the file to begin
-    size_t bytesToRead = sizeof(BootSector); // how many bytes to be read from the file
-    ssize_t bytesRead; // how many bytes were actually read from the file
-    char buffer[bytesToRead]; // buffer where the file bytes are temporarily read to
-    BootSector bootSector; // declaration of the BootSector struct.
+    int fd;                                     // The fat16 image
+    off_t offset = 0;                           // How many bytes from the start of the file to begin
+    size_t bytesToRead = sizeof(BootSector);    // How many bytes to be read from the file
+    ssize_t bytesRead;                          // How many bytes were actually read from the file
+    char buffer[bytesToRead];                   // Buffer where the file bytes are temporarily read to
+    BootSector bootSector;                      // Declaration of the BootSector struct.
 
 
-    // opens file
-    fd = open("fat16.img", O_RDONLY);
-
-    // seeks to the start of the file for the bootsector
-    lseek(fd, offset, SEEK_SET);
-
-    // reads the file from start the the end of the boot sector
-    bytesRead = read(fd, buffer, bytesToRead);
-    
-    // copies its contents to the correct struct fields
-    memcpy(&bootSector, buffer, bytesToRead);
+    fd = open("fat16.img", O_RDONLY);           // Open file
+    lseek(fd, offset, SEEK_SET);                // Seek to the start of the file for the bootsector
+    bytesRead = read(fd, buffer, bytesToRead);  // Read the file from start the the end of the boot sector
+    memcpy(&bootSector, buffer, bytesToRead);   // Copy its contents to the correct struct fields
 
 
-    // prints said bytes (for testing)
+    // Print bootsector fields
     printf("BS_jmpBoot: ");
     for (size_t i = 0; i < sizeof(bootSector.BS_jmpBoot); i++) {
         printf("%02X, ", bootSector.BS_jmpBoot[i]);
@@ -91,6 +84,5 @@ int main() {
     printf("\n");
 
 
-    // close file
-    close(fd);
+    close(fd);      // Close file
 }
